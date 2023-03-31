2feb0ae52074baae170fad4acbb2f215f7fe4a4d
jeffclaybrook <48254646+jeffclaybrook@users.noreply.github.com>
2023-03-31 14:26:24 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const body = document.body;
+const gallery = document.querySelector('.gallery');
 const dialog = document.querySelector('dialog');
 const loader = document.querySelector('.loader');
 const apiKey = 'NiNRjQTmXyxC2698onGYqbzDUeQJ4MHpTcoAiWni7DkTnXfvoCpCzqDL';
@@ -12,9 +13,11 @@ let photoIndex = 1;
 function showToast(hex) {
     const toast = document.querySelector('.toast');
     toast.classList.add('visible');
+    toast.setAttribute('aria-hidden', 'false');
     toast.innerText = `Copied ${hex}`;
     setTimeout(() => {
         toast.classList.remove('visible');
+        toast.setAttribute('aria-hidden', 'true');
         toast.innerText = '';
     }, 1500);
 }
@@ -32,7 +35,7 @@ function downloadImage(image) {
         const anchor = document.createElement('a');
         anchor.href = URL.createObjectURL(blob);
         anchor.download = new Date().getTime();
-        document.body.appendChild(anchor);
+        body.appendChild(anchor);
         anchor.click();
         anchor.remove();
     }).catch(() => console.log('Failed to download image'));
@@ -41,19 +44,31 @@ function downloadImage(image) {
 
 function closeDialog() {
     dialog.classList.remove('expanded');
+    dialog.setAttribute('aria-hidden', 'true');
     dialog.querySelector('.dialog-container').innerHTML = '';
     body.style.overflow = 'auto';
 }
 
 
-function openDialog(ab, bc, cd, de, ef, fg, gh, hi, ij) {
+function openDialog(
+    ab,
+    bc,
+    cd,
+    de,
+    ef,
+    fg,
+    gh,
+    hi,
+    ij
+) {
     body.style.overflow = 'hidden';
+    dialog.setAttribute('aria-hidden', 'false');
     dialog.classList.add('expanded');
     dialog.querySelector('.dialog-container').innerHTML += `
     <div class="dialog-header">
         <i class="material-symbols-rounded">photo_camera</i>
         <h2 id="${ef}">${ab}</h2>
-        <button onclick="closeDialog()">
+        <button onclick="closeDialog()" aria-label="Close image">
             <i class="material-symbols-rounded">close</i>
         </button>
     </div>
@@ -62,60 +77,110 @@ function openDialog(ab, bc, cd, de, ef, fg, gh, hi, ij) {
     </div>
     <div class="dialog-footer">
         <div class="column">
-            <a href="${de}" target="_blank">
+            <a href="${de}" target="_blank" aria-label="Photographer profile">
                 <i class="material-symbols-rounded">person</i>
             </a>
         </div>
         <div class="column">
-            <a href="${bc}" target="_blank">
+            <a href="${ij}" target="_blank" aria-label="Open image in new tab">
                 <i class="material-symbols-rounded">open_in_new</i>
             </a>
         </div>
         <div class="column">
-            <button data-color="${hi}" onclick="copyPalette('${hi}')">
+            <button data-color="${hi}" onclick="copyPalette('${hi}')" aria-label="Copy HEX code">
                 <i class="material-symbols-rounded" style="color: ${hi}">launcher_assistant_on</i>
             </button>
         </div>
         <div class="column">
-            <button data-img="${bc}" onclick="downloadImage('${bc}')">
+            <button data-img="${bc}" onclick="downloadImage('${bc}')" aria-label="Download image">
                 <i class="material-symbols-rounded">download</i>
             </buton>
         </div>
     </div>
-    `
+    `;
 }
 
 
-function createGallery(photos) {
-    const ul = document.querySelector('.gallery');
-    const gallery = photos.map((photo, i) => {
-        const { alt, avg_color, height, id, photographer, photographer_id, photographer_url, src: { large2x }, width } = photo;
-        photoIndex++
-        i = photoIndex;
+async function createGallery(data) {
+    const items = data.map(item => {
+        const {
+            id,
+            width,
+            height,
+            url,
+            photographer,
+            photographer_url,
+            photographer_id,
+            avg_color,
+            src,
+            alt
+        } = item;
+        const {
+            large2x,
+            large,
+            medium,
+            small,
+            landscape
+        } = src;
         return `
-        <li class="item" id="${id}" onclick="openDialog('${photographer}', '${large2x}', '${alt}', '${photographer_url}', '${photographer_id}', '${width}', '${height}', '${avg_color}')">
-            <img src="${large2x}" alt="${photographer}">
+        <li
+            class="item"
+            onclick="openDialog(
+                '${photographer}',
+                '${large2x}',
+                '${alt}',
+                '${photographer_url}',
+                '${photographer_id}',
+                '${width}',
+                '${height}',
+                '${avg_color}',
+                '${url}'
+            )"
+        >
+            <img
+                alt="${alt}"
+                id="${id}"
+                data-width="${width}"
+                data-height="${height}"
+                data-url="${url}"
+                data-avg_color="${avg_color}"
+                src="${large2x}"
+                srcset="
+                    ${large2x},
+                    ${large},
+                    ${medium},
+                    ${small},
+                    ${landscape}
+                "
+            >
             <div class="item-details">
                 <i class="material-symbols-rounded">photo_camera</i>
-                <h3>(${i}) ${photographer}</h3>
+                <h3
+                    data-photographer="${photographer}"
+                    data-photographer_url="${photographer_url}"
+                    data-photographer_id="${photographer_id}"
+                >${photographer}</h3>
             </div>
         </li>
         `
     }).join('');
-    ul.innerHTML += `${gallery}`;
+    gallery.innerHTML += `${items}`;
 }
 
 
-function getData(url) {
-    fetch(url, {
-        headers: { Authorization: apiKey }
-    }).then(res => res.json()).then(data => {
+async function getData(url) {
+    const settings = { headers: { Authorization: apiKey } };
+    try {
+        const res = await fetch(url, settings);
+        const data = await res.json();
         createGallery(data.photos);
-    }).catch(() => console.log('Error fetching images'))
+    } catch {
+        console.log('Error fetching data');
+    }
 }
 
 
-function getMoreData() {
+async function getMoreData() {
     currentPage++;
     let url = `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`;
     getData(url);
@@ -128,8 +193,10 @@ getData(`https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPag
 window.addEventListener('scroll', () => {
     if (window.scrollY + window.innerHeight >= document.body.offsetHeight - 15) {
         loader.classList.add('loading');
+        loader.innerHTML = `<div class="dots"></div>`
         setTimeout(() => {
             loader.classList.remove('loading');
+            loader.innerHTML = '';
             getMoreData();
         }, 1500);
     }
